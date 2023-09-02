6a5b6d5962a7cb6c6139bc35119fb2b9c5ae7ddc
jgomez <jgomez@wktksoft.com>
2023-09-02 18:27:47 +0200

    create eslint.json, change prettier config, change consumerGroup, create and working on castSheets schema

--- src/domain/repositories/mongo.repository.js
+++ src/domain/repositories/mongo.repository.js
@@ -1,30 +1,31 @@
-const config = require('config-yml');
-const mongoose = require('mongoose');
-const magic = require('../../utils/magic');
-const consumer = require('../entities/entity-consumer');
-const consumerGroup = require('../entities/entity-consumerGroup');
+const config = require('config-yml')
+const mongoose = require('mongoose')
+const magic = require('../../utils/magic')
+const consumer = require('../entities/entity-consumer')
+const consumerGroup = require('../entities/entity-consumerGroup')
+const castSheets = require('../entities/entity-castSheets')
 
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
 
-dotenv.config();
+dotenv.config()
 
-let db = {};
+let db = {}
 
 if (config.db.mongodb && config.db.mongodb.length > 0) {
   config.db.mongodb.map((c) => {
     mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    db[c.nameconn] = {};
-    db[c.nameconn].conn = mongoose;
-    db[c.nameconn].Consumer = consumer(mongoose);
-    db[c.nameconn].ConsumerGroup = consumerGroup(mongoose);
-   
-  });
-  exports.db = db;
-  
-  magic.LogInfo('Conectado a la base de datos');
+      useUnifiedTopology: true
+    })
+    db[c.nameconn] = {}
+    db[c.nameconn].conn = mongoose
+    db[c.nameconn].Consumer = consumer(mongoose)
+    db[c.nameconn].ConsumerGroup = consumerGroup(mongoose)
+    db[c.nameconn].CastSheets = castSheets(mongoose)
+  })
+  exports.db = db
+
+  magic.LogInfo('Conectado a la base de datos')
 } else {
-  magic.LogDanger('No existe la base de datos');
+  magic.LogDanger('No existe la base de datos')
 }
