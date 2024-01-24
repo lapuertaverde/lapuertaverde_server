67974e3516386c2be3ab9701de104935862d4504
John Doe <fontanadeoro1983@gmail.com>
2024-01-24 20:14:15 +0100

    create isAdminAuth middleware

---
+++ src/middlewares/adminAuth.middleware.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const isAdminAuth = (req, res, next) => {
+  const authorization = req.headers.authorization
+
+  if (!authorization) return res.json(setError(401, 'Not authorized'))
+
+  const splits = authorization.split(' ')
+
+  if (splits.length !== 2 && splits[0] !== 'Bearer') return res.json(setError(400, 'Not Bearer'))
+
+  const jwtStringify = splits[1]
+
+  jwt.verify(jwtStringify, process.env.SECRET, (err, verifiedJwt) => {
+    if (err) {
+      setError(500, 'Token invalid')
+    } else {
+      const authority = {
+        id: verifiedJwt.id,
+        name: verifiedJwt.name,
+        role: verifiedJwt.role
+      }
+
+      req.authority = authority
+
+      next()
+    }
+  })
+}
+
+export default isAdminAuth
